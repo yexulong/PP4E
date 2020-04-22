8c3c4dfad4ac0b10208853c0dbb011007373f687
yexulong <yexulong@foxmail.com>
2020-04-22 13:21:06 +0000

    learning os.spawn 04-22

---
+++ System/Processes/makewords.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+print('spam')
+print('eggs')
+print('ham')
+
---
+++ System/Processes/spawnv.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import os, sys
+
+for i in range(10):
+    if sys.platform[:3] == 'win':
+        pypath = sys.executable
+        os.spawnv(os.P_NOWAIT, pypath, ('python3', 'child.py', str(i)))
+    else:
+        pid = os.fork()
+        if pid != 0:
+            print('Process %d spawned' % pid)
+        else:
+            os.execlp('python3', 'python3', 'child.py', str(i))
+print('Main process exiting.')
